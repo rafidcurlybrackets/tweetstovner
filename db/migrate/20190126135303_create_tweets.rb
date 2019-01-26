dd681f1c7988b1b675b362b89b048c535fe39a5e
Rafid <rafid@hoda.io>
2019-01-26 14:25:05 +0000

    Start project

---
+++ db/migrate/20190126135303_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.text :body
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
